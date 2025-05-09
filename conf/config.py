b4a3cd6f01fac171933beb4f222fe7901c667c23
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2025-05-09 18:27:00 +0200

    changes

--- conf/config.py
+++ conf/config.py
@@ -4,9 +4,9 @@ import os
 load_dotenv()
 
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "567234")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "hw02")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
 SQLALCHEMY_DATABASE_URL = (
